c7a4327e62c482f180282a50771277d660a6edca
lukovnikov <lukovnikov@outlook.com>
2016-03-03 19:58:42 +0100

    added docs

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 [![Build Status](https://travis-ci.org/lukovnikov/teafacto.svg?branch=master)](https://travis-ci.org/lukovnikov/teafacto)
 [![Coverage Status](https://coveralls.io/repos/github/lukovnikov/teafacto/badge.svg?branch=master)](https://coveralls.io/github/lukovnikov/teafacto?branch=master)
+[![Documentation Status](https://readthedocs.org/projects/teafacto/badge/?version=latest)](http://teafacto.readthedocs.org/en/latest/?badge=latest)
 
 :tea::factory:
--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -288,6 +288,14 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
 
 class Mock(MagicMock):
     @classmethod
