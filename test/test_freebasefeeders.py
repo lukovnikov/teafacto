03ed0a080d5ab39e614bc2f5739d4b79190c835c
lukovnikov <lukovnikov@outlook.com>
2016-04-06 21:37:20 +0200

    fb lexicon learning :banana:

--- test/test_freebasefeeders.py
+++ test/test_freebasefeeders.py
@@ -1,15 +1,16 @@
 from unittest import TestCase
 from teafacto.feeders.freebasefeeders import FBLexDataFeedsMaker, getentdict, getglovedict
+import os
 
 
 class TestFreebaseLexFeeder(TestCase):
     def test_getentdic(self):
-        d, maxid = getentdict("../../data/freebase/entdic.small.map", top=50)
+        d, maxid = getentdict(os.path.join(os.path.dirname(__file__), "../data/freebase/entdic.small.map"), top=50)
         self.assertEqual(maxid, 52)
         self.assertEqual(max(d.values()), maxid)
 
     def test_getglovedict(self):
-        d, maxi = getglovedict("../../data/glove/miniglove.50d.txt")
+        d, maxi = getglovedict(os.path.join(os.path.dirname(__file__), "../data/glove/miniglove.50d.txt"))
         self.assertEqual(maxi, 4001)
         self.assertEqual(max(d.values()), maxi)
 
