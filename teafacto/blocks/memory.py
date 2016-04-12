721ddf4ce9f30500ef2680681505d02ea144e81f
lukovnikov <lukovnikov@outlook.com>
2016-04-13 00:26:32 +0200

    implemented memory block :banana:

--- teafacto/blocks/memory.py
+++ teafacto/blocks/memory.py
@@ -8,17 +8,23 @@ class MemoryBlock(Embedder):
     The transformation should generate a vector representation for each element.
     Further, they act as Vector Embedding blocks.
 
+    If data argument is not specified, a variable will be assumed to be given to the apply() function.
     """
     def __init__(self, block=None, data=None, indim=200, outdim=50, **kw):
-        assert(block is not None and data is not None)
-        # TODO: convert non-Var or non-Val data to Val
-        if not isinstance(data, (Var, Val)):
+        assert(block is not None)
+        self.datavar = data is None
+        if not isinstance(data, (Var, Val)) and data is not None:
             data = Val(data)
-        assert(isinstance(data, (Var, Val)) and isinstance(block, Block))
+        assert((isinstance(data, (Var, Val)) or data is None) and isinstance(block, Block))
+        self.data = data
         super(MemoryBlock, self).__init__(indim, outdim, **kw)      # outdim = outdim of the contained block
         self.payload = block
-        self.innervar = block(data)     # innervar: (indim, outdim)
-        pass
+        self.innervar = self.payload(data) if data is not None else None    # innervar: (indim, outdim)
 
-    def apply(self, idxs):  # idxs: ints of (batsize,)
+    def apply(self, idxs, datavar=None):     # idxs: ints of (batsize,)
+        if self.innervar is None:
+            assert(isinstance(datavar, (Var, Val)))
+            self.innervar = self.payload(datavar)
+        else:
+            assert(datavar is None)
         return self.innervar[idxs, :]
--- test/test_memoryblock.py
+++ test/test_memoryblock.py
@@ -30,10 +30,61 @@ class TestMemoryBlock(TestCase):
             IdxToOneHot(vocsize=invocabsize),
             gru
         )
-        memb = MemoryBlock(payload, data, outdim=encdim)
+        memb = MemoryBlock(payload, data, indim=invocabsize, outdim=encdim)
         idxs = [0, 2, 5]
         memory_element = memb.predict(idxs)
         self.assertEqual(memory_element.shape, (len(idxs), encdim))
         gruparams = set([getattr(gru, pname) for pname in gru.paramnames])
         allparams = set(memb.output.allparams)
         self.assertEqual(gruparams.intersection(allparams), allparams)
+
+    def test_memory_block_with_seq_encoder_dynamic(self):
+        invocabsize = 5
+        memsize = 10
+        seqlen = 3
+        encdim = 13
+        data = np.random.randint(0, invocabsize, (memsize, seqlen))
+        gru = GRU(dim=invocabsize, innerdim=encdim)
+        payload = SeqEncoder(
+            IdxToOneHot(vocsize=invocabsize),
+            gru
+        )
+        dynmemb = MemoryBlock(payload, outdim=encdim)
+        idxs = [0, 2, 5]
+        memory_element = dynmemb.predict(idxs, data)
+        self.assertEqual(memory_element.shape, (len(idxs), encdim))
+        gruparams = set([getattr(gru, pname) for pname in gru.paramnames])
+        allparams = set(dynmemb.output.allparams)
+        self.assertEqual(gruparams.intersection(allparams), allparams)
+
+        statmemb = MemoryBlock(payload, data, outdim=encdim)
+        statpred = statmemb.predict(idxs)
+        self.assertTrue(np.allclose(statpred, memory_element))
+
+    def test_memory_block_with_seq_encoder_dynamic_fail(self):
+        invocabsize = 5
+        encdim = 13
+        gru = GRU(dim=invocabsize, innerdim=encdim)
+        payload = SeqEncoder(
+            IdxToOneHot(vocsize=invocabsize),
+            gru
+        )
+        dynmemb = MemoryBlock(payload, outdim=encdim)
+        idxs = [0, 2, 5]
+        self.assertRaises(AssertionError, lambda: dynmemb.predict(idxs))
+
+    def test_memory_block_with_seq_encoder_static_fail(self):
+        invocabsize = 5
+        memsize = 10
+        seqlen = 3
+        encdim = 13
+        data = np.random.randint(0, invocabsize, (memsize, seqlen))
+        gru = GRU(dim=invocabsize, innerdim=encdim)
+        payload = SeqEncoder(
+            IdxToOneHot(vocsize=invocabsize),
+            gru
+        )
+        memb = MemoryBlock(payload, data, outdim=encdim)
+        idxs = [0, 2, 5]
+        self.assertRaises(AssertionError, lambda: memb.predict(idxs, data))
+
