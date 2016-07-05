624139007402cd7810795bc49d0476c6321360d6
lukovnikov <lukovnikov@outlook.com>
2016-07-05 23:52:07 +0200

    * simplequestions dataset processing

--- data/simplequestions/tomat.word.mem.py
+++ data/simplequestions/tomat.word.mem.py
@@ -11,7 +11,7 @@ def run(trainp="fb_train.tsv",
         outp="datamat.word.mem.fb2m.pkl",
         entnames="subjnames_fb2m.map",
         rellist="rels_fb2m.list",
-        maxnamelen=np.infty):
+        maxnamelen=30):
     # worddic
     worddic = {"<RARE>": 0}
     def updateworddic(*words):
