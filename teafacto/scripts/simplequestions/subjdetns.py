fab21ebcd8397a88903d8792214238c116653d96
lukovnikov <lukovnikov@outlook.com>
2016-07-13 21:59:12 +0200

    * qsdf

--- teafacto/scripts/simplequestions/subjdetns.py
+++ teafacto/scripts/simplequestions/subjdetns.py
@@ -302,6 +302,7 @@ def run(
 
     eval = SubjRankEval(scorer, worddic=worddic, entdic=entdic, metrics=[ClassAccuracy(), RecallAt(10)])
     eval.eval(testdata, testgold, transform=PreProcf)
+    tt.msg("tested dummy")
     #embed()
     # trainer config and training
     scorer = scorer.nstrain([traindata, traingold]).transform(PreProcf(entmat))\
