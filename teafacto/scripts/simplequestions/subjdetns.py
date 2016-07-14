c52d8a202f499a6318425bc83c67735e4cda782d
lukovnikov <lukovnikov@outlook.com>
2016-07-15 00:50:17 +0200

    ticktock live fix

--- teafacto/scripts/simplequestions/subjdetns.py
+++ teafacto/scripts/simplequestions/subjdetns.py
@@ -45,7 +45,7 @@ class SubjRankEval(object):
             numcans = len(cans[i])
             predinp = [np.repeat(np.expand_dims(data[i, :], axis=0), numcans, axis=0),
                        np.asarray(cans[i], dtype="int32")]
-            print predinp
+            print predinp, i
             predinpscores = predictor(*predinp)      # (numcans,)
             ranking = sorted(zip(cans[i], list(predinpscores)),
                              key=lambda (x, y): y)
