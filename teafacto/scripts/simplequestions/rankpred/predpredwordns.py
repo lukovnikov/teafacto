e70b6dca4ec429098099535b5b201477e82170c0
lukovnikov <lukovnikov@outlook.com>
2016-09-19 21:45:20 +0200

    at least n cans in eval

--- teafacto/scripts/simplequestions/rankpred/predpredwordns.py
+++ teafacto/scripts/simplequestions/rankpred/predpredwordns.py
@@ -244,7 +244,7 @@ def run(epochs=50,
         if len(cans) < atleastcan:
             extracans = list(np.random.randint(0, numents, (atleastcan-len(cans),)))
             extracans = list(set(extracans).difference(set(cans)))
-            cans = cans + extracans[:max(0, atleastcan - len(cans))]
+            cans = cans + extracans[:max(0, min(len(extracans), atleastcan - len(cans)))]
             #print len(cans), cans
 
         #cans = set(cans)
