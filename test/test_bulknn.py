466c1f9eab6eb85bd98ce80408316593445d0877
lukovnikov <lukovnikov@outlook.com>
2017-02-06 23:45:47 +0100

    * BulkNN initial implementation almost finished

--- test/test_bulknn.py
+++ test/test_bulknn.py
@@ -8,20 +8,29 @@ class BulkNNTest(TestCase):
     def test_init_mem_shape(self):
         inpvocsize = 5
         outvocsize = 7
-        impembdim = 10
+        inpembdim = 10
         memembdim = 12
-        inpdim = 15
-        memdim = 15
+        inpdim = [15]
+        memdim = [15]
         memlen = 17
-        coredims = [20, 30]
+        writedim = 19
+        lastcoredim = inpdim[-1] + memdim[-1] + memdim[-1] \
+                      + writedim + 1 + 1
+        coredims = [40, lastcoredim]     # last dim must match interface when explicit interface
 
         batsize = 13
         seqlen = 11
 
-        m = SimpleBulkNN(inpvocsize=inpvocsize, inpembdim=impembdim, inpencinnerdim=inpdim,
-                         memvocsize=outvocsize, memembdim=memembdim, memencinnerdim=memdim,
-                         memlen=memlen, coredims=coredims,
-                         explicit_interface=True, write_value_dim=15)
+        m = SimpleBulkNN(inpvocsize=inpvocsize,
+                         inpembdim=inpembdim,
+                         inpencinnerdim=inpdim,
+                         memvocsize=outvocsize,
+                         memembdim=memembdim,
+                         memencinnerdim=memdim,
+                         memlen=memlen,
+                         coredims=coredims,
+                         explicit_interface=True,
+                         write_value_dim=writedim)
 
         d = np.random.randint(0, inpvocsize, (batsize, seqlen))
         '''
