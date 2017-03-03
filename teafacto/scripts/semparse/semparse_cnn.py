bc0b38c044c0ea19358c91df7676e648da43d9cb
lukovnikov <lukovnikov@outlook.com>
2017-03-03 22:42:54 +0100

    semparse

--- teafacto/scripts/semparse/semparse_cnn.py
+++ teafacto/scripts/semparse/semparse_cnn.py
@@ -94,6 +94,7 @@ def run(numbats=50,
         amat_test = np.concatenate([amat_test, np.ones((amat_test.shape[0], amat_train.shape[1] - amat_test.shape[1]),
                                                        dtype="int32") * maskid], axis=1)
         tt.tock("concat genned")
+
     if inspectdata:
         def pp_train(i):
             print wordids2string(qmat_train[i], {v: k for k, v in qdic.items()}, 0)
@@ -135,9 +136,9 @@ def run(numbats=50,
 
     tt.tick("training")
 
-    decoder.train([amat_train[:, :-1], qmat_train], amat_train[:, 1:]) \
+    decoder.train([qmat_train[:, :-1], amat_train], qmat_train[:, 1:]) \
         .cross_entropy().seq_accuracy().adadelta(lr=lr).grad_total_norm(1.) \
-        .validate_on([amat_test[:, :-1], qmat_test], amat_test[:, 1:]) \
+        .validate_on([qmat_test[:, :-1], amat_test], qmat_test[:, 1:]) \
         .cross_entropy().seq_accuracy() \
         .train(numbats, epochs)
 
