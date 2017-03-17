229a46905009a6969601241c67cb7f821a7348f7
lukovnikov <lukovnikov@outlook.com>
2017-03-17 21:53:44 +0100

    Chunking script * added pos task and eval

--- teafacto/scripts/tagging/pos.py
+++ teafacto/scripts/tagging/pos.py
@@ -312,7 +312,7 @@ def run(
             .adadelta(lr=lr).grad_total_norm(gradnorm).exp_mov_avg(0.99)\
             .split_validate(splits=10)\
             .cross_entropy().seq_accuracy().extvalid(extvalid)\
-            .takebest(f=lambda x: x[3])\
+            .takebest(f=lambda x: x[3], no=True)\
             .train(numbats=numbats, epochs=epochs, _skiptrain=debugvalid)
     else:
         tt.msg("skipping training")
