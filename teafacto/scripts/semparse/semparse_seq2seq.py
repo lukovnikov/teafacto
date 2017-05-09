0012ddd1576d5f897959101514351b6ebba8ddd9
lukovnikov <lukovnikov@outlook.com>
2017-05-09 18:44:21 +0200

    * added gated eucl attention weights

--- teafacto/scripts/semparse/semparse_seq2seq.py
+++ teafacto/scripts/semparse/semparse_seq2seq.py
@@ -197,7 +197,8 @@ def run(numbats=50,
     tt.tick("training")
 
     decoder.train([amat_train[:, :-1], qmat_train], amat_train[:, 1:]) \
-        .cross_entropy().cross_entropy(cemode="allmean").seq_accuracy().adadelta(lr=lr).grad_total_norm(gradnorm) \
+        .cross_entropy().cross_entropy(cemode="allmean").seq_accuracy()\
+        .adadelta(lr=lr).grad_total_norm(gradnorm) \
         .validate_on([amat_test[:, :-1], qmat_test], amat_test[:, 1:]) \
         .cross_entropy().cross_entropy(cemode="allmean").seq_accuracy() \
         .train(numbats, epochs)
