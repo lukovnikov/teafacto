2919eb29b9b8189809a5000890146641688c7bfd
lukovnikov <lukovnikov@outlook.com>
2017-06-16 21:03:56 +0200

    * dgtn added dummy for find check

--- teafacto/scripts/got/dgtn.py
+++ teafacto/scripts/got/dgtn.py
@@ -253,7 +253,7 @@ def run_trainfind(lr=0.1,
     tt.tick("training")
 
     m.train([lmat], gold) \
-        .adadelta(lr=lr).loss(PWPointerLoss(balanced=True)).grad_total_norm(5.) \
+        .adadelta(lr=lr).loss(PWPointerLoss(balanced=False)).grad_total_norm(5.) \
         .train(numbats, epochs)
 
     tt.tock("trained")
