d0f5a46b12002cff214589cb1256415e11db3b51
lukovnikov <lukovnikov@outlook.com>
2017-07-24 21:47:19 +0200

    * webqa semparse full script fix

--- teafacto/scripts/webqa/semparse.py
+++ teafacto/scripts/webqa/semparse.py
@@ -176,13 +176,15 @@ def run(p="webqa.data.loaded.pkl",
 
     m = asblock(m_fun)
 
-    def runtest(total=1639):        # see http://anthology.aclweb.org/P16-2033
+    def runtest(total=1639, inspect=False):        # see http://anthology.aclweb.org/P16-2033
         tt.tick("running test")
         acc = 0
         tot = test_nl_mat.shape[0]
         i = 0
         j = 0
         step = 100
+        rev_nl_dic = {v: k for k, v in nl_dic.items()}
+        rev_fl_dic = {v: k for k, v in fl_dic.items()}
         while j < test_nl_mat.shape[0]:
             tt.progress(i, tot, live=True)
             j = min(i + step, test_nl_mat.shape[0])
@@ -191,6 +193,15 @@ def run(p="webqa.data.loaded.pkl",
             eqs = np.all(test_fl_mat[i:j, 1:] == testpred, axis=1)
             acc += eqs.sum()
             i += step
+            if inspect:
+                def pp(q, g, a):
+                    for qe, ge, ae in zip(q, g, a):
+                        print "Question:\t ".join([rev_nl_dic[qex] for qex in qe if qex != 0])
+                        print "Golden:\t ".join([rev_fl_dic[gex] for gex in ge if gex != 0])
+                        print "Predicted:\t ".join([rev_fl_dic[aex] for aex in ae if aex != 0])
+                pp(test_nl_mat[i:j], test_fl_mat[i:j], testpred[i:j])
+                raw_input("press to continue")
+
         tt.tock("test run")
         tt.msg("Accuracy on test: {} ({}/{}); [all:] {} ({}/{})".format(acc * 1.0 / tot, acc, tot, acc * 1.0 / total, acc, total))
 
