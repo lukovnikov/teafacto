853424a4feb0cd7eb7584aef9a0151fb4b1555c5
lukovnikov <lukovnikov@outlook.com>
2017-08-14 21:54:03 +0200

    * added mnist rnn

--- teafacto/scripts/bnloss/mnistrnn.py
+++ teafacto/scripts/bnloss/mnistrnn.py
@@ -10,6 +10,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import GRU
 from keras import backend as K
 from teafacto.util import ticktock
 
@@ -45,15 +46,8 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(GRU(128))
+model.add(GRU(128))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
---
+++ teafacto/scripts/mnist/rnn.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+
+from teafacto.blocks.seq import RNNSeqEncoder
+from teafacto.util import ticktock
+
+
+def main(
+    # Hyper Parameters
+        sequence_length = 28,
+        input_size = 28,
+        hidden_size = 128,
+        num_layers = 2,
+        num_classes = 10,
+        batch_size = 300,
+        num_epochs = 2,
+        learning_rate = 0.01,
+
+        gpu = False,
+        mode = "stack"     # "nn" or "qrnn" or "stack"
+    ):
+
+
+    tt = ticktock("script")
+    tt.msg("using q: {}".format(mode))
+    # MNIST Dataset
+    train_dataset = dsets.MNIST(root='../../../data/mnist/',
+                                train=True,
+                                transform=transforms.ToTensor(),
+                                download=True)
+
+    test_dataset = dsets.MNIST(root='../../../data/mnist/',
+                               train=False,
+                               transform=transforms.ToTensor())
+
+    # Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
+
+    # RNN Model (Many-to-One)
+    class RNN(nn.Module):
+        def __init__(self, input_size, hidden_size, num_layers, num_classes):
+            super(RNN, self).__init__()
+            self.hidden_size = hidden_size
+            self.num_layers = num_layers
+            if mode == "qrnn":
+                tt.msg("using q.RNN")
+                self.rnn = RecStack(*[GRU(input_size, hidden_size)]+
+                                     [GRU(hidden_size, hidden_size) for i in range(num_layers-1)])\
+                            .to_layer().return_all()
+            elif mode == "nn":
+                tt.msg("using nn.RNN")
+                self.rnn = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+            elif mode == "stack":
+                self.rnn = RNNStack(
+                    *([nn.GRU(input_size, hidden_size, 1, batch_first=True)] +
+                      [nn.GRU(hidden_size, hidden_size, 1, batch_first=True) for i in range(num_layers - 1)]
+                    )
+                )
+            self.fc = nn.Linear(hidden_size, num_classes)
+
+        def forward(self, x):
+            # Set initial states
+            h0 = q.var(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).cuda(crit=x).v
+            #c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+
+            # Forward propagate RNN
+            if mode == "qrnn":
+                out = self.rnn(x)
+            else:
+                out, _ = self.rnn(x, h0)
+
+            # Decode hidden state of last time step
+            out = self.fc(out[:, -1, :])
+            return out
+    if gpu:
+        q.var.all_cuda = True
+    rnn = RNN(input_size, hidden_size, num_layers, num_classes)
+    if gpu:
+        rnn.cuda()
+
+
+    # Loss and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    if gpu:
+        criterion.cuda()
+    optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
+
+    tt.msg("training")
+    # Train the Model
+    for epoch in range(num_epochs):
+        tt.tick()
+        btt = ticktock("batch")
+        btt.tick()
+        for i, (images, labels) in enumerate(train_loader):
+            #btt.tick("doing batch")
+            images = q.var(images.view(-1, sequence_length, input_size)).cuda(crit=gpu).v
+            labels = q.var(labels).cuda(crit=gpu).v
+
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()
+            outputs = rnn(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 100 == 0:
+                btt.tock("100 batches done")
+                print ('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
+                       %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+                btt.tick()
+            #tt.tock("batch done")
+        tt.tock("epoch {} done".format(epoch))
+    # Test the Model
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = q.var(images.view(-1, sequence_length, input_size)).cuda(crit=gpu).v
+        labels = q.var(labels).cuda(crit=gpu).v
+        outputs = rnn(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels.data).sum()
+
+    print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+
+    # Save the Model
+    torch.save(rnn.state_dict(), 'rnn.pkl')
+
+
+if __name__ == "__main__":
+    q.argprun(main)
