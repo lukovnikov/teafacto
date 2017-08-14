7c1f046fd480268b64ad6ac16a1406f37ebad400
lukovnikov <lukovnikov@outlook.com>
2017-08-14 22:17:02 +0200

    * added mnist rnn

--- teafacto/scripts/bnloss/mnistrnn.py
+++ teafacto/scripts/bnloss/mnistrnn.py
@@ -12,48 +12,53 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import GRU
 from keras import backend as K
-from teafacto.util import ticktock
+from teafacto.util import ticktock, argprun
 
-batch_size = 100
-num_classes = 10
-epochs = 2
+def main(
+    batch_size = 100,
+    num_classes = 10,
+    epochs = 2,
+    ):
 
-# input image dimensions
-img_rows, img_cols = 28, 28
+    # input image dimensions
+    img_rows, img_cols = 28, 28
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # the data, shuffled and split between train and test sets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols)
-input_shape = (img_rows, img_cols)
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols)
+    input_shape = (img_rows, img_cols)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(GRU(128, return_sequences=True, input_shape=input_shape))
-model.add(GRU(128))
-model.add(Dense(num_classes, activation='softmax'))
+    model = Sequential()
+    model.add(GRU(128, return_sequences=True, input_shape=input_shape))
+    model.add(GRU(128))
+    model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+if __name__ == "__main__":
+    argprun(main)
