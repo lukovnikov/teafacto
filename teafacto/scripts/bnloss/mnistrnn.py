9ed04153d7d06f66cfcce49e2d9d26e4e263e003
lukovnikov <lukovnikov@outlook.com>
2017-08-14 21:58:57 +0200

    * added mnist rnn

--- teafacto/scripts/bnloss/mnistrnn.py
+++ teafacto/scripts/bnloss/mnistrnn.py
@@ -14,9 +14,9 @@ from keras.layers import GRU
 from keras import backend as K
 from teafacto.util import ticktock
 
-batch_size = 128
+batch_size = 100
 num_classes = 10
-epochs = 12
+epochs = 2
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -24,14 +24,8 @@ img_rows, img_cols = 28, 28
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -46,7 +40,7 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(GRU(128))
+model.add(GRU(128, return_sequences=True))
 model.add(GRU(128))
 model.add(Dense(num_classes, activation='softmax'))
 
